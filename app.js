9295906ee207e6906fd83378c39b8aa4986b4405
Martina Kurkova <martinanoha@seznam.cz>
2025-04-26 17:51:33 +0200

    hotovo

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-console.log('Funguju!')
-
-const textPozpatku = (text) => {
-	return text.split('').reverse().join('')
-}
+document.querySelectorAll('li').forEach((item) => {
+	const text = item.textContent.toLowerCase().replace(/\s/g, '');
+	const reversed = text.split('').reverse().join('');
+  
+	if (text === reversed) {
+	  item.classList.add('palindrom');
+	}
+  });
 
 // textPozpatku('Ahoj světe') // 'etěvs johA'
 // textPozpatku('Kajak') // 'kajaK'
